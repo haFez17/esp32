369d22f4629b18c2a2a195d35b69dec80594b636
haFez17 <rabbitcool515@gmail.com>
2025-02-26 20:51:38 +0200

    Добавление регистра и логина

--- ESP32/server/server.py
+++ ESP32/server/server.py
@@ -31,7 +31,6 @@ async def home():
 async def register_page(request: Request):
     return templates.TemplateResponse("register.html", {"request": request, "title": "Регистрация"})
 
-
 @app.post("/register/")
 async def register(request: Request, username: str = Form(...), password: str = Form(...), mac_address: str = Form(...)):
     if database.save_user(username, password, mac_address):
@@ -42,50 +41,43 @@ async def register(request: Request, username: str = Form(...), password: str = 
         "error": "Логин или MAC-адрес уже используется!"
     })
 
-
 @app.get("/login/")
 async def login_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request, "title": "Вход"})
 
-
 @app.post("/login/")
 async def login(request: Request, username: str = Form(...), password: str = Form(...)):
     if database.authenticate_user(username, password):
-        return RedirectResponse(url="/translate/", status_code=303)  # ✅ После логина перекидывает на страницу перевода
+        return RedirectResponse(url="/translation/", status_code=303)
     return templates.TemplateResponse("login.html", {
         "request": request,
         "title": "Ошибка",
         "error": "Неверный логин или пароль!"
     })
 
-
 @app.get("/translation/")
 async def translate_page(request: Request):
-    data = list(database.collection.find({}, {"_id": 0}))  # Получаем последние переводы
-    return templates.TemplateResponse("translaion.html", {
+    data = list(database.collection.find({}, {"_id": 0}))
+    return templates.TemplateResponse("translation.html", {
         "request": request,
         "title": "Перевод",
         "translations": data
     })
 
-
 @app.post("/upload/")
 async def upload_images(file: UploadFile = File(...), target_language: str = "en"):
     image = Image.open(file.file)
     text = pytesseract.image_to_string(image, lang="eng")
-
     corrected_text = correct_text(text)
     translated_text = translator.translate(corrected_text, dest=target_language).text
-
     database.save_data(corrected_text, translated_text, target_language)
-
     return {"original_text": corrected_text, "translated_text": translated_text}
-
 
 def correct_text(text):
     words = text.split()
     corrected_words = [spell.correction(word) or word for word in words]
     return " ".join(corrected_words)
+
 
 
 if __name__ == '__main__':
