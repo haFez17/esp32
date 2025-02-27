ce9710dd310ed09dec1dba70a80deff9efe77cd2
haFez17 <rabbitcool515@gmail.com>
2025-02-27 10:51:31 +0200

    изменение базы данных

--- ESP32/server/database.py
+++ ESP32/server/database.py
@@ -1,8 +1,31 @@
 from pymongo import MongoClient
+from passlib.hash import bcrypt
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["translations_db"]
 collection = db["translations"]
+users_collection = db["users"]
+
+
+def save_user(username, password, mac_address):
+    if users_collection.find_one({"username": username}) or users_collection.find_one({"mac_address": mac_address}):
+        return False  # Логин или MAC-адрес уже зарегистрирован
+
+    hashed_password = bcrypt.hash(password)
+    users_collection.insert_one({
+        "username": username,
+        "password": hashed_password,
+        "mac_address": mac_address
+    })
+    return True
+
+
+def authenticate_user(username, password):
+    user = users_collection.find_one({"username": username})
+    if user and bcrypt.verify(password, user["password"]):
+        return True
+    return False
+
 
 def save_data(original, translated, lang):
     data = {
@@ -11,4 +34,3 @@ def save_data(original, translated, lang):
         "language": lang
     }
     collection.insert_one(data)
-
